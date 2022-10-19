26a81d57fd1bff3e43775df30babe7656851a44e
Thirupathi-702 <115494239+Thirupathi-702@users.noreply.github.com>
2022-10-19 22:11:50 +0530

    Create script.js

---
+++ Quote Generator/script.js
@@ -0,0 +1,13 @@
+const btn = document.querySelector('#btn')
+
+const quotes = document.querySelector('.quotes')
+
+btn.addEventListener('click', generateQuote)
+
+generateQuote()
+
+function generateQuote() {
+    fetch('http://quotes.stormconsultancy.co.uk/random.json')
+  .then(response => response.json())
+  .then(data => quotes.innerText=data.quote);
+}
